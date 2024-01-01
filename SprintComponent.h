b705bdb0452ee0d0e14aa29d9fd49f5e394ae5fe
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2024-01-01 23:04:37 +0000

    Update SprintComponent.h

--- SprintComponent.h
+++ SprintComponent.h
@@ -21,8 +21,8 @@ class UInputAction;
 * Sprint Component Class. Performs behaviors related to sprinting.
 * Receives input from EnhancedInputSystem Plugin and assigns locomotion values based on definitions in this class.
 * Make sure to assign a UInputAction to define the bindings for this behavior!
-*	@since 17/01/2023
-*	@author JDSherbert
+* @since 17/01/2023
+* @author JDSherbert
 */
 UCLASS(ClassGroup = "Sherbert", Blueprintable, meta = (BlueprintSpawnableComponent))
 class SHERBERT_API USprintComponent : public UActorComponent
@@ -84,9 +84,8 @@ protected:
 
 	/**
 	* Initializer method.
-	*	@return void
-	*	@since 17/01/2023
-	*	@author JDSherbert
+	* @since 17/01/2023
+	* @author JDSherbert
 	*/
 	void Init();
 
@@ -94,10 +93,9 @@ public:
 
 	/**
 	* Sets Sprint Mode. Assigns values based on truth. Calls OnSprintStart and OnSprintStop events.
-	*	@param bActive : Set this actor to either Sprint or not sprint.
-	*	@return void
-	*	@since 17/01/2023
-	*	@author JDSherbert
+	* @param bActive : Set this actor to either Sprint or not sprint.
+	* @since 17/01/2023
+	* @author JDSherbert
 	*/
 	UFUNCTION(BlueprintCallable, Category = "Sherbert|Component|Sprint")
 	void Sprint(const bool bActive = false); //! Not being called by the enhanced input system
@@ -109,90 +107,90 @@ public:
 
 	/**
 	* Gets Sprint Mode Truth.
-	*	@return bool bSprinting
-	*	@since 17/01/2023
-	*	@author JDSherbert
+	* @return bool bSprinting
+	* @since 17/01/2023
+	* @author JDSherbert
 	*/
 	UFUNCTION(BlueprintPure, Category = "Sherbert|Component|Sprint")
 	FORCEINLINE bool Get_IsSprinting() const { return bSprinting; }
 
 	/**
 	* Sets Sprint Mode Truth.
-	*	@return bool bSprinting
+	* @return bool bSprinting
 	* @since 17/01/2023
-	*	@author JDSherbert
+	* @author JDSherbert
 	*/
 	UFUNCTION(BlueprintCallable, Category = "Sherbert|Component|Sprint")
 	FORCEINLINE bool Set_IsSprinting(const bool bActive = false) { bSprinting = bActive; return bSprinting; }
 
 	/**
 	* Gets Sprint Speed used by the component as defined.
-	*	@return float SprintSpeed
+	* @return float SprintSpeed
 	* @since 17/01/2023
-	*	@author JDSherbert
+	* @author JDSherbert
 	*/
 	UFUNCTION(BlueprintPure, Category = "Sherbert|Component|Sprint")
 	FORCEINLINE float Get_SprintSpeed() const { return SprintSpeed; }
 
 	/**
 	* Sets a new Sprint Speed for the component to use when sprinting.
-	*	@return float SprintSpeed
-	* 	@since 17/01/2023
-	*	@author JDSherbert
+	* @return float SprintSpeed
+	* @since 17/01/2023
+	* @author JDSherbert
 	*/
 	UFUNCTION(BlueprintCallable, Category = "Sherbert|Component|Sprint")
 	FORCEINLINE float Set_SprintSpeed(const float NewSprintSpeed) { SprintSpeed = NewSprintSpeed; return SprintSpeed; }
 
 	/**
 	* Gets Rotation Rate used by the component as defined.
-	*	@return FRotator SprintRotationRate
-	* 	@since 17/01/2023
-	*	@author JDSherbert
+	* @return FRotator SprintRotationRate
+	* @since 17/01/2023
+	* @author JDSherbert
 	*/
 	UFUNCTION(BlueprintPure, Category = "Sherbert|Component|Sprint")
 	FORCEINLINE FRotator Get_RotationRate() const { return SprintRotationRate; }
 
 	/**
 	* Sets a new Rotation Speed for the component to use when sprinting.
-	*	@return FRotator SprintRotationRate
-	* 	@since 17/01/2023
-	*	@author JDSherbert
+	* @return FRotator SprintRotationRate
+	* @since 17/01/2023
+	* @author JDSherbert
 	*/
 	UFUNCTION(BlueprintCallable, Category = "Sherbert|Component|Sprint")
 	FORCEINLINE FRotator Set_RotationRate(const FRotator NewSprintRotationRate) { SprintRotationRate = NewSprintRotationRate; return SprintRotationRate; }
 
 	/**
 	* Gets Sprint Key Hold Time.
-	*	@return float SprintKeyHoldTime
-	* 	@since 17/01/2023
-	*	@author JDSherbert
+	* @return float SprintKeyHoldTime
+	* @since 17/01/2023
+	* @author JDSherbert
 	*/
 	UFUNCTION(BlueprintPure, Category = "Sherbert|Component|Sprint")
 	FORCEINLINE float Get_KeyHoldTime() const { return SprintKeyHoldTime; }
 
 	/**
 	* Gets Movement Component reference used by the component as acquired on construction.
-	*	@return UCharacterMovementComponent* MovementComponent
-	*	@since 17/01/2023
-	*	@author JDSherbert
+	* @return UCharacterMovementComponent* MovementComponent
+	* @since 17/01/2023
+	* @author JDSherbert
 	*/
 	UFUNCTION(BlueprintPure, Category = "Sherbert|Component|Sprint")
 	FORCEINLINE UCharacterMovementComponent* Get_MovementComponent() const { return MovementComponent; }
 
 	/**
 	* Gets Sprint Key Input Action.
-	*	@return UInputAction* InputAction_Sprint
+	* @return UInputAction* InputAction_Sprint
 	* @since 17/01/2023
-	*	@author JDSherbert
+	* @author JDSherbert
 	*/
 	UFUNCTION(BlueprintPure, Category = "Sherbert|Component|Sprint")
 	FORCEINLINE UInputAction* Get_InputAction() const { return InputAction_Sprint; }
 
 	/**
 	* Sets a new Sprint Key Input Action.
-	*	@return UInputAction* InputAction_Sprint
+	* @return UInputAction* InputAction_Sprint
 	* @since 17/01/2023
-	*	@author JDSherbert
+	* @author JDSherbert
 	*/
 	UFUNCTION(BlueprintCallable, Category = "Sherbert|Component|Sprint")
 	FORCEINLINE UInputAction* Set_InputAction(UInputAction* NewInputAction) { InputAction_Sprint = NewInputAction; return InputAction_Sprint; }
@@ -201,9 +199,9 @@ public:
 
 	/**
 	* Replicated. Sets new Sprint values.
-	*	@return void
+	* @return void
 	* @since 13/02/2023
-	*	@author JDSherbert
+	* @author JDSherbert
 	*/
 	UFUNCTION()
 	void OnRep_bSprinting();
@@ -212,18 +210,18 @@ public:
 
 	/**
 	* Event: Triggers when Sprint Key Input Action is received.
-	*	@return void
+	* @return void
 	* @since 17/01/2023
-	*	@author JDSherbert
+	* @author JDSherbert
 	*/
 	UFUNCTION(BlueprintImplementableEvent, Category = "Sherbert|Component|Sprint")
 	void Event_OnSprintStart();
 
 	/**
 	* Event: Triggers when Sprint Key Input Action is released.
-	*	@return void
+	* @return void
 	* @since 17/01/2023
-	*	@author JDSherbert
+	* @author JDSherbert
 	*/
 	UFUNCTION(BlueprintImplementableEvent, Category = "Sherbert|Component|Sprint")
 	void Event_OnSprintStop();
